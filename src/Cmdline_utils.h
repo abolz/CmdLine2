966d1b092055f7b6b2605b8f48e7d8a58b003fc6
abolz <lt.morris.schaffer@googlemail.com>
2017-09-09 00:00:29 +0200

    Rewrite

--- src/Cmdline_utils.h
+++ src/Cmdline_utils.h
@@ -21,242 +21,244 @@
 #ifndef CL_CMDLINE_UTILS_H
 #define CL_CMDLINE_UTILS_H 1
 
+#include <cassert>
 #include <iterator>
+#include <string>
+#include <type_traits>
 
 namespace cl {
-inline namespace v2 {
 
-// Parses a single argument from a command line string.
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+
+// Parse arguments from a command line string.
 // Using Unix-style escaping.
-template <typename InpIt, typename Buffer>
-InpIt TokenizeSingleArgUnix(InpIt first, InpIt last, Buffer& buffer)
+struct GenArgsUnix
 {
-    //
-    // See:
-    //
-    // http://www.gnu.org/software/bash/manual/bashref.html#Quoting
-    // http://wiki.bash-hackers.org/syntax/quoting
-    //
-
-    int quote_char = 0;
-
-    for (/**/; first != last; ++first)
+    template <typename InpIt, typename EndIt, typename YieldChar>
+    InpIt operator()(InpIt next, EndIt last, YieldChar yield_char) const
     {
-        auto ch = *first;
+        //using value_type = typename std::iterator_traits<InpIt>::value_type;
+        using value_type = std::remove_const_t<std::remove_reference_t<decltype(*next)>>;
 
-        // Quoting a single character using the backslash?
-        if (quote_char == '\\')
+        //
+        // See:
+        //
+        // http://www.gnu.org/software/bash/manual/bashref.html#Quoting
+        // http://wiki.bash-hackers.org/syntax/quoting
+        //
+
+        value_type quote_char = '\0';
+
+        // Skip leading space
+        for ( ; next != last; ++next)
         {
-            buffer.push_back(ch);
-            quote_char = 0;
-        }
-        // Currently quoting using ' or "?
-        else if (quote_char && ch != quote_char)
-        {
-            buffer.push_back(ch);
-        }
-        // Toggle quoting?
-        else if (ch == '\'' || ch == '"' || ch == '\\')
-        {
-            quote_char = quote_char ? 0 : ch;
-        }
-        // Arguments are separated by whitespace
-        else if (ch == ' ' || ch == '\t')
-        {
-            return ++first;
-        }
-        else
-        {
-            buffer.push_back(ch);
-        }
-    }
+            auto ch = *next;
 
-    return first;
-}
-
-// Parses a command line string and returns a list of command line arguments.
-// Using Unix-style escaping.
-template <typename InpIt, typename Rhs>
-void TokenizeUnix(InpIt first, InpIt last, Rhs& out)
-{
-    if (first == last)
-        return;
-
-    typename Rhs::value_type buffer;
-
-    while (first != last)
-    {
-        buffer.clear();
-
-        first = cl::TokenizeSingleArgUnix(first, last, buffer);
-
-        if (!buffer.empty())
-            out.push_back(buffer);
-    }
-}
-
-// Parse off the program filename.
-// For Windows.
-template <typename InpIt, typename Buffer>
-InpIt ParseProgramFilenameWindows(InpIt first, InpIt last, Buffer& buffer)
-{
-    if (first == last)
-        return first;
-
-    bool quoting = (*first == '"');
-
-    if (quoting)
-        ++first;
-
-    for (/**/; first != last; ++first)
-    {
-        auto ch = *first;
-
-        if (ch == '"' && quoting)
-        {
-            ++first;
-            break;
+            if (ch != ' ' && ch != '\t')
+                break;
         }
 
-        if ((ch == ' ' || ch == '\t') && !quoting)
+        for ( ; next != last; ++next)
         {
-            ++first;
-            break;
-        }
+            auto ch = *next;
 
-        buffer.push_back(ch);
-    }
-
-    return first;
-}
-
-// Parses a single argument from a command line string.
-// Using Windows-style escaping.
-template <typename InpIt, typename Buffer>
-InpIt TokenizeSingleArgWindows(InpIt first, InpIt last, Buffer& buffer, int& quoting)
-{
-    int recently_closed = 0;
-    int num_backslashes = 0;
-
-    quoting = 0;
-
-    for (/**/; first != last; ++first)
-    {
-        auto ch = *first;
-
-        if (ch == '"' && recently_closed)
-        {
-            recently_closed = 0;
-
-            // If a closing " is followed immediately by another ", the 2nd " is
-            // accepted literally and added to the parameter.
             //
-            // See:
-            // http://www.daviddeley.com/autohotkey/parameters/parameters.htm#WINCRULESDOC
-
-            buffer.push_back('"');
-
-#if 0 // post 2008
-            quoting = 1;
-#endif
-        }
-        else if (ch == '"')
-        {
-            // If an even number of backslashes is followed by a double
-            // quotation mark, one backslash is placed in the argv array for
-            // every pair of backslashes, and the double quotation mark is
-            // interpreted as a string delimiter.
+            // Quoting a single character using the backslash?
             //
-            // If an odd number of backslashes is followed by a double quotation
-            // mark, one backslash is placed in the argv array for every pair of
-            // backslashes, and the double quotation mark is "escaped" by the
-            // remaining backslash, causing a literal double quotation mark (")
-            // to be placed in argv.
-
-            int even = (num_backslashes % 2) == 0;
-
-            num_backslashes /= 2;
-            for (/**/; num_backslashes != 0; --num_backslashes)
-                buffer.push_back('\\');
-
-            if (even)
+            if (quote_char == '\\')
             {
-                recently_closed = quoting; // Remember if this is a closing "
-                quoting = !quoting;
+                yield_char(ch);
+                quote_char = '\0';
+            }
+            //
+            // Currently quoting using ' or "?
+            //
+            else if (quote_char != '\0' && ch != quote_char)
+            {
+                yield_char(ch);
+            }
+            //
+            // Toggle quoting?
+            //
+            else if (ch == '\'' || ch == '"' || ch == '\\')
+            {
+                quote_char = (quote_char != '\0') ? '\0' : ch;
+            }
+            //
+            // Arguments are separated by whitespace
+            //
+            else if (ch == ' ' || ch == '\t')
+            {
+                ++next;
+                break;
             }
             else
             {
-                buffer.push_back('"');
+                yield_char(ch);
             }
         }
-        else if (ch == '\\')
+
+        return next;
+    }
+};
+
+// Parse off the program filename.
+// For Windows.
+struct ParseProgramNameWindows
+{
+    template <typename InpIt, typename EndIt, typename YieldChar>
+    InpIt operator()(InpIt first, EndIt last, YieldChar yield_char) const
+    {
+        if (first == last)
+            return first;
+
+        bool quoting = (*first == '"');
+
+        if (quoting)
+            ++first;
+
+        for ( ; first != last; ++first)
         {
-            recently_closed = 0;
+            auto ch = *first;
 
-            ++num_backslashes;
-        }
-        else
-        {
-            recently_closed = 0;
-
-            // Backslashes are interpreted literally, unless they immediately
-            // precede a double quotation mark.
-
-            for (/**/; num_backslashes != 0; --num_backslashes)
-                buffer.push_back('\\');
-
-            if ((ch == ' ' || ch == '\t') && !quoting)
+            if (ch == '"' && quoting)
             {
-                // Arguments are delimited by white space, which is either a
-                // space or a tab.
-                //
-                // A string surrounded by double quotation marks ("string") is
-                // interpreted as single argument, regardless of white space
-                // contained within. A quoted string can be embedded in an
-                // argument.
-
                 ++first;
                 break;
             }
 
-            buffer.push_back(ch);
+            if ((ch == ' ' || ch == '\t') && !quoting)
+            {
+                ++first;
+                break;
+            }
+
+            yield_char(ch);
         }
+
+        return first;
     }
+};
 
-    return first;
-}
-
-// Parses a command line string and returns a list of command line arguments.
+// Parses a single argument from a command line string.
 // Using Windows-style escaping.
-template <typename InpIt, typename Rhs>
-void TokenizeWindows(InpIt first, InpIt last, Rhs& out, bool parse_program_name = true)
+struct GenArgsWindows
 {
-    if (first == last)
-        return;
-
-    typename Rhs::value_type buffer;
-
-    if (parse_program_name)
+    template <typename InpIt, typename EndIt, typename YieldChar>
+    InpIt operator()(InpIt next, EndIt last, YieldChar yield_char) const
     {
-        first = cl::ParseProgramFilenameWindows(first, last, buffer);
-        out.push_back(buffer);
-    }
+        bool   quoting         = false;
+        bool   recently_closed = false;
+        size_t num_backslashes = 0;
 
-    while (first != last)
-    {
-        int quoting = 0;
-
-        first = cl::TokenizeSingleArgWindows(first, last, buffer, quoting);
-
-        if (!buffer.empty() || quoting)
+        // Skip leading space
+        for ( ; next != last; ++next)
         {
-            out.push_back(buffer);
-            buffer.clear();
-        }
-    }
-}
+            auto ch = *next;
 
-// Quote a single command line argument. Using Windows-style escaping.
+            if (ch != ' ' && ch != '\t')
+                break;
+        }
+
+        for ( ; next != last; ++next)
+        {
+            auto ch = *next;
+
+            if (ch == '"' && recently_closed)
+            {
+                recently_closed = false;
+
+                //
+                // If a closing " is followed immediately by another ", the 2nd
+                // " is accepted literally and added to the parameter.
+                //
+                // See:
+                // http://www.daviddeley.com/autohotkey/parameters/parameters.htm#WINCRULESDOC
+                //
+
+                yield_char('"');
+            }
+            else if (ch == '"')
+            {
+                //
+                // If an even number of backslashes is followed by a double
+                // quotation mark, one backslash is placed in the argv array for
+                // every pair of backslashes, and the double quotation mark is
+                // interpreted as a string delimiter.
+                //
+                // If an odd number of backslashes is followed by a double
+                // quotation mark, one backslash is placed in the argv array for
+                // every pair of backslashes, and the double quotation mark is
+                // "escaped" by the remaining backslash, causing a literal
+                // double quotation mark (") to be placed in argv.
+                //
+
+                bool even = (num_backslashes % 2) == 0;
+
+                num_backslashes /= 2;
+                for (/**/; num_backslashes != 0; --num_backslashes)
+                {
+                    yield_char('\\');
+                }
+
+                if (even)
+                {
+                    recently_closed = quoting; // Remember if this is a closing "
+                    quoting = !quoting;
+                }
+                else
+                {
+                    yield_char('"');
+                }
+            }
+            else if (ch == '\\')
+            {
+                recently_closed = false;
+
+                ++num_backslashes;
+            }
+            else
+            {
+                recently_closed = false;
+
+                //
+                // Backslashes are interpreted literally, unless they
+                // immediately precede a double quotation mark.
+                //
+
+                for (/**/; num_backslashes != 0; --num_backslashes)
+                {
+                    yield_char('\\');
+                }
+
+                if ((ch == ' ' || ch == '\t') && !quoting)
+                {
+                    //
+                    // Arguments are delimited by white space, which is either a
+                    // space or a tab.
+                    //
+                    // A string surrounded by double quotation marks ("string")
+                    // is interpreted as single argument, regardless of white
+                    // space contained within. A quoted string can be embedded
+                    // in an argument.
+                    //
+
+                    ++next;
+                    break;
+                }
+
+                yield_char(ch);
+            }
+        }
+
+        return next;
+    }
+};
+
+// Quote a single command line argument.
+// Using Windows-style escaping.
 //
 // See:
 // http://blogs.msdn.com/b/twistylittlepassagesallalike/archive/2011/04/23/everyone-quotes-arguments-the-wrong-way.aspx
@@ -265,82 +267,159 @@ void TokenizeWindows(InpIt first, InpIt last, Rhs& out, bool parse_program_name 
 // CommandLineToArgvW will return the argument string unchanged. Arguments in a
 // command line should be separated by spaces; this function does not add these
 // spaces.
-template <typename InpIt, typename Buffer>
-void QuoteSingleArgWindows(InpIt first, InpIt last, Buffer& buffer)
+struct QuoteArgsWindows
 {
-    int num_backslashes = 0;
-
-    buffer.push_back('"');
-
-    for (/**/; first != last; ++first)
+    template <typename InpIt, typename EndIt, typename YieldChar>
+    void operator()(InpIt first, EndIt last, YieldChar yield_char)
     {
-        auto ch = *first;
+        if (first == last)
+            return;
 
-        if (ch == '\\')
+        auto yield_bs = [&](size_t count) // Append COUNT backslashes
         {
-            ++num_backslashes;
-        }
-        else if (ch == '"')
+            for ( ; count != 0; --count)
+            {
+                yield_char('\\');
+            }
+        };
+
+        yield_char('"');
+
+        for ( ; ; ++first)
         {
-            // Escape all backslashes and the following double quotation mark.
+            size_t num_backslashes = 0;
 
-            for (/**/; num_backslashes != 0; --num_backslashes)
-                buffer.push_back('\\');
+            for ( ; first != last; ++first)
+            {
+                auto ch = *first;
 
-            buffer.push_back('\\');
+                if (ch != '\\')
+                    break;
+
+                ++num_backslashes;
+            }
+
+            if (first == last)
+            {
+                //
+                // Escape all backslashes, but let the terminating double
+                // quotation mark we add below be interpreted as a
+                // metacharacter.
+                //
+
+                yield_bs(num_backslashes * 2);
+                break;
+            }
+            else if (*first == '"')
+            {
+                //
+                // Escape all backslashes and the following double quotation
+                // mark.
+                //
+
+                yield_bs(num_backslashes * 2 + 1);
+                yield_char('"');
+            }
+            else
+            {
+                //
+                // Backslashes aren't special here.
+                //
+
+                yield_bs(num_backslashes);
+                yield_char(*first);
+            }
         }
-        else
-        {
-            num_backslashes = 0; // Backslashes aren't special here.
-        }
 
-        buffer.push_back(ch);
+        yield_char('"');
     }
+};
 
-    // Escape all backslashes, but let the terminating double quotation mark we
-    // add below be interpreted as a metacharacter.
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+#if 0
 
-    for (/**/; num_backslashes != 0; --num_backslashes)
-        buffer.push_back('\\');
+class ArgvEnd {
+    //bool operator==(ArgvEnd) const { return true; }
+    //bool operator!=(ArgvEnd) const { return false; }
+};
 
-    buffer.push_back('"');
+inline ArgvEnd argv_end()
+{
+    return {};
 }
 
-// Quote command line arguments. Using Windows-style escaping.
-template <typename InpIt, typename Buffer>
-InpIt QuoteArgsWindows(InpIt first, InpIt last, Buffer& buffer)
+template <typename InpIt, typename EndIt = InpIt, typename Generator = cl::GenArgsUnix>
+class ArgvIterator
 {
-    using std::begin;
-    using std::end;
+    static_assert(
+        std::is_convertible<decltype(*std::declval<InpIt>()), char>::value,
+        "Incompatible iterator value_type");
 
-    for (/**/; first != last; ++first)
+    using Buffer = std::string;
+
+    Buffer      arg;
+    InpIt       first;
+    InpIt       next;
+    EndIt const last;
+
+public:
+    using iterator_category = std::input_iterator_tag;
+    using value_type        = Buffer;
+    using reference         = Buffer /*const*/&;
+    using pointer           = Buffer /*const*/*;
+    using difference_type   = ptrdiff_t;
+
+public:
+    ArgvIterator(InpIt f, EndIt l)
+        : first(f)
+        , next(f)
+        , last(l)
     {
-        auto I = begin(*first);
-        auto E = end(*first);
-
-        if (I == E)
-        {
-            // If a command line string ends while currently quoting,
-            // CommandLineToArgvW will add the current argument to the argv
-            // array regardless whether the current argument is empty or not.
-            //
-            // So if we have an empty argument here add an opening " and return.
-            // This should be the last argument though...
-
-            buffer.push_back('"');
-            break;
-        }
-
-        // Append the current argument
-        cl::QuoteSingleArgWindows(I, E, buffer);
-
-        buffer.push_back(' ');
     }
 
-    return first;
+    reference operator*()
+    {
+        assert(first != last);
+        if (first == next)
+        {
+            arg.clear();
+            next = Generator{}(first, last, [&](auto ch) { arg.push_back(ch); });
+        }
+
+        return arg;
+    }
+
+    pointer operator->()
+    {
+        return &(operator*());
+    }
+
+    ArgvIterator& operator++()
+    {
+        first = next;
+        return *this;
+    }
+
+    bool operator==(ArgvEnd) const { return first == last; }
+    bool operator!=(ArgvEnd) const { return first != last; }
+};
+
+template <typename InpIt, typename EndIt>
+ArgvIterator<InpIt, EndIt> argv_begin(InpIt first, EndIt last)
+{
+    return ArgvIterator<InpIt, EndIt>(first, last);
 }
 
-} // namespace v2
-} // namespace cl
+template <typename InpIt, typename EndIt, typename Generator>
+ArgvIterator<InpIt, EndIt, Generator> argv_begin(InpIt first, EndIt last, Generator)
+{
+    return ArgvIterator<InpIt, EndIt, Generator>(first, last);
+}
 
 #endif
+
+} // namespace cl
+
+#endif // CL_CMDLINE_UTILS_H
