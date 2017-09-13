842b5625fd0168538c73cbb501e5f5e1ea804d55
abolz <lt.morris.schaffer@googlemail.com>
2017-09-13 23:01:12 +0200

    Update example

--- test/Example.cc
+++ test/Example.cc
@@ -1,82 +1,68 @@
 #include "../src/Cmdline.h"
-//#include <iostream>
-#include <unordered_map>
-//#include <sstream>
 
-enum class Simpson {
-    homer,
-    marge,
-    bart,
-    lisa,
-    maggie,
-};
+#include <unordered_map>
+
+enum class Standard { cxx11, cxx14, cxx17 };
+
+static bool flag = false;
+static int i = 0;
+static Standard standard = Standard::cxx11;
+static std::unordered_map<std::string, int> string_map;
+static std::vector<std::string> input_files;
 
 int main(int argc, char* argv[])
 {
-    bool show_help = false;
-    bool flag = false;
-    int i = 0;
-    double f = 0.0;
-    std::vector<std::string> input_files;
-    Simpson simpson;
-    std::unordered_map<std::string, int> hmap;
-
     cl::Cmdline cmd;
 
-    /*auto opt_h =*/ cmd.Add("h|help|?", "Display this message", cl::Assign(show_help));
-
-    cmd.Add("f", "A boolean flag", 
-        cl::Assign(flag), 
+    // OK   "-f"
+    // OK   "-f=1"
+    // OK   "-f=no"
+    // FAIL "-f=abcd"
+    cmd.Add("f", "A boolean flag",
+        cl::Assign(flag),
         cl::Opt::zero_or_more, cl::MayGroup::yes, cl::Arg::optional);
 
-    auto IsEven = [](cl::ParseContext& ctx, auto i) {
-        if (i % 2 == 0)
-            return true;
-        ctx.cmdline->EmitDiag(cl::Diagnostic::error, ctx.index, "Argument must be an even integer");
-        return false;
-    };
-
-    auto Times2 = [](cl::ParseContext const& /*ctx*/, int& i) { i += i; return true; };
-
-    cmd.Add("i|ints", "Some even ints in the range [0,6]",
-        cl::Assign(i, cl::check::InRange(0, 6), IsEven, Times2),
+    // OK   "-i 0 -i=1,2 -i=0x3 -i 4,5,6"
+    // FAIL "-i=-1"
+    // FAIL "-i=0x7"
+    // FAIL "-i=abc"
+    cmd.Add("i|ints", "Some ints in the range [0,6]",
+        cl::Assign(i, cl::CheckInRange(0, 6)),
         cl::Opt::zero_or_more, cl::Arg::required, cl::CommaSeparatedArg::yes);
 
-    cmd.Add("floats", "Some floats in the range [0,pi]",
-        cl::Assign(f, cl::check::InRange(0, 3.1415)),
-        cl::Opt::zero_or_more, cl::Arg::required, cl::CommaSeparatedArg::yes);
+    // OK   "-std c++11"
+    // OK   "-std=c++17"
+    // FAIL "-std"
+    // FAIL "-std=c++20"
+    // FAIL "-std=c++11 -std=c++11"
+    cmd.Add("std", "C++ standard version",
+        cl::Map(standard, {{ "c++11", Standard::cxx11 },
+                           { "c++14", Standard::cxx14 },
+                           { "c++17", Standard::cxx17 }}),
+        cl::Opt::optional, cl::Arg::required);
 
+    // OK   "-si=eins:1"
+    // OK   "-si eins:0x1,zwei:2"
+    // FAIL "-si"
+    // FAIL "-si null"
+    // FAIL "-si null:abcd"
+    cmd.Add("si", "String-Int pairs",
+        cl::PushBack(string_map),
+        cl::Arg::required, cl::Opt::zero_or_more, cl::CommaSeparatedArg::yes);
+
+    // OK   "eins zwei drei"
+    // FAIL ""
     cmd.Add("input-files", "List of input files",
         cl::PushBack(input_files),
         cl::Opt::one_or_more, cl::Positional::yes);
 
-    cmd.Add("simpson", "One of the Simpsons",
-        cl::Map(simpson, {{"homer",    Simpson::homer },
-                          {"marge",    Simpson::marge },
-                          {"bart",     Simpson::bart  },
-                          {"el barto", Simpson::bart  },
-                          {"lisa",     Simpson::lisa  },
-                          {"maggie",   Simpson::maggie}}),
-        cl::Opt::zero_or_more, cl::Arg::required);
-
-    cmd.Add("si", "String-Int pairs",
-        cl::PushBack(hmap),
-        cl::Opt::zero_or_more, cl::Arg::required);
-
     bool const ok = cmd.Parse(argv + 1, argv + argc);
-    //if (show_help)
-    //{
-    //    std::cerr << cmd.HelpMessage("Test");
-    //    return 0;
-    //}
     if (!ok)
     {
-        cmd.PrintDiag();
-        cmd.PrintHelp("Example"); //cmd.PrintHelpMessage(argv[0]);
+        cmd.PrintDiag();        // Print error message(s) to stderr.
+        cmd.PrintHelp(argv[0]); // Print help message to stderr.
         return -1;
     }
-
-    //std::cout << "i = " << i << "\n";
 
     return 0;
 }
