69bd7e380f64377724de78e353c979627391a849
Alexander Bolz <lt.morris.schaffer@googlemail.com>
2018-11-27 21:18:03 +0100

    Fix example

--- test/Example_subcommands.cc
+++ test/Example_subcommands.cc
@@ -17,37 +17,37 @@ static bool progr = false;
 using ArgIterator = char const* const*;
 using ParseResult = cl::Cmdline::ParseResult<ArgIterator>;
 
-static ParseResult Parse(cl::Cmdline& cli, ArgIterator next, ArgIterator last, cl::string_view program_name)
+static ParseResult Parse(cl::Cmdline& cli, ArgIterator next, ArgIterator last)
 {
     auto const res = cli.Parse(next, last);
     if (!res) {
         cli.PrintDiag();
-        cli.PrintHelp(program_name);
+        cli.PrintHelp();
     }
     return res;
 }
 
 static ParseResult ParseMakeCommand(ArgIterator next, ArgIterator last)
 {
-    cl::Cmdline cli;
+    cl::Cmdline cli("finder make");
 
     cli.Add("wordfile", "", cl::Var(input), cl::Positional::yes, cl::HasArg::required, cl::NumOpts::one_or_more);
     cli.Add("dict", "", cl::Var(dict), cl::HasArg::required, cl::NumOpts::required);
     cli.Add("progress", "show progress", cl::Var(progr));
 
-    return Parse(cli, next, last, "finder make");
+    return Parse(cli, next, last);
 }
 
 static ParseResult ParseFindCommand(ArgIterator next, ArgIterator last)
 {
-    cl::Cmdline cli;
+    cl::Cmdline cli("finder find");
 
     cli.Add("infile", "", cl::Var(input), cl::NumOpts::one_or_more, cl::HasArg::required, cl::Positional::yes);
     cli.Add("dict", "", cl::Var(dict), cl::HasArg::required, cl::NumOpts::required);
     cli.Add("o", "write to file instead of stdout", cl::Var(out), cl::HasArg::required);
     cli.Add("split|nosplit", "(do not) split output", cl::Flag(split, /*inverse_prefix*/ "no"));
 
-    return Parse(cli, next, last, "finder find");
+    return Parse(cli, next, last);
 }
 
 static bool ParseCommandLine(ArgIterator next, ArgIterator last)
