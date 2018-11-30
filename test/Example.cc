d7bab1c52db38d3e633df6639b2a76997c5e07f7
Alexander Bolz <lt.morris.schaffer@googlemail.com>
2018-11-30 18:19:46 +0100

    Update example

--- test/Example.cc
+++ test/Example.cc
@@ -1,5 +1,3 @@
-#define CL_WINDOWS_CONSOLE_COLORS 1
-#define CL_ANSI_CONSOLE_COLORS 1
 #include "Cmdline.h"
 
 enum class Standard { cxx11, cxx14, cxx17 };
@@ -9,17 +7,12 @@ static int i = 0;
 static Standard standard = Standard::cxx11;
 static std::vector<std::string> input_files;
 
-#if 0// _MSC_VER
-int wmain(int argc, wchar_t* argv[])
-#else
 int main(int argc, char* argv[])
-#endif
 {
     cl::Cmdline cli("Example", "Does nothing useful");
 
     cli.Add("v", "Increase output verbosity",
-        cl::Var(verbose),
-        cl::Multiple::yes, cl::MayGroup::yes, cl::Arg::optional);
+        cl::Var(verbose), cl::Multiple::yes, cl::MayGroup::yes, cl::Arg::optional);
 
     cli.Add("i|ints", "Some ints in the range [0,6]",
         cl::Var(i, cl::check::InRange(0, 6)),
@@ -30,8 +23,7 @@ int main(int argc, char* argv[])
         cl::Arg::required);
 
     cli.Add("input-files", "List of input files",
-        cl::Var(input_files),
-        cl::Required::yes, cl::Multiple::yes, cl::Positional::yes);
+        cl::Var(input_files), cl::Required::yes, cl::Multiple::yes, cl::Positional::yes);
 
     auto const res = cli.Parse(argv + 1, argv + argc);
     if (!res)
